f35b9dd3e807d7608e20b5b15e550ce50348487d
Anton <a.kochetkov@gmail.com>
2014-10-23 23:21:03 +0100

    saving of the DatasetTimp data matrix

--- Hdf5Interface/src/main/java/org/glotaran/hdf5interface/Hdf5DatasetTimp.java
+++ Hdf5Interface/src/main/java/org/glotaran/hdf5interface/Hdf5DatasetTimp.java
@@ -8,7 +8,11 @@ package org.glotaran.hdf5interface;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import ncsa.hdf.hdf5lib.exceptions.HDF5Exception;
+import ncsa.hdf.object.Dataset;
+import ncsa.hdf.object.Datatype;
 import ncsa.hdf.object.FileFormat;
+import ncsa.hdf.object.Group;
 //import ncsa.hdf.object.h5.H5File;
 import ncsa.hdf.object.h5.*;
 import org.glotaran.core.models.structures.DatasetTimp;
@@ -22,7 +26,24 @@ public class Hdf5DatasetTimp {
 
     private static int init = 0;
     
+    private static void init() {
+        try {
+            Class fileclass = Class.forName("ncsa.hdf.object.h5.H5File");
+            FileFormat fileformat = (FileFormat) fileclass.newInstance();
+            if (fileformat != null) {
+                FileFormat.addFileFormat("HDF5", fileformat);
+                init = 1;
+            }
+        } catch (Throwable err) {
+            CoreInformationMessages.HDF5Info(err.getMessage());
+        }
+    }
+    
     public static DatasetTimp load(File file) throws FileNotFoundException {
+        if (0 == init) {
+            init();
+        }
+        
         DatasetTimp dataset = new DatasetTimp();
 
         dataset.setDatasetName("Test HDF5");
@@ -31,36 +52,50 @@ public class Hdf5DatasetTimp {
     }
 
     public static void save(File file, DatasetTimp dataset) throws IOException {
-
+        H5File hdfFile = null;
+                
         if (0 == init) {
-            try {
-                Class fileclass = Class.forName("ncsa.hdf.object.h5.H5File");
-                FileFormat fileformat = (FileFormat) fileclass.newInstance();
-                if (fileformat != null) {
-                    FileFormat.addFileFormat("HDF5", fileformat);
-                    init = 1;
-                }
-            } catch (Throwable err) {
-                CoreInformationMessages.HDF5Info(err.getMessage());
-            }
+            init();
         }
         // Retrieve an instance of the implementing class for the HDF5 format
         FileFormat fileFormat = FileFormat.getFileFormat(FileFormat.FILE_TYPE_HDF5);
         if (fileFormat == null) {
             CoreInformationMessages.HDF5Info("Cannot find HDF5 FileFormat.");
-            //return;
+            return;
         }
 
+        
         try {
-            H5File testFile = (H5File) fileFormat.createFile(file.getCanonicalPath(), FileFormat.FILE_CREATE_DELETE);
+            hdfFile = (H5File) fileFormat.createFile(file.getCanonicalPath(), FileFormat.FILE_CREATE_DELETE);
             // Check for error condition and report.
-            if (testFile == null) {
+            if (hdfFile == null) {
                 CoreInformationMessages.HDF5Info("Failed to create file: " + file.getName());
+                return;
             }
+            hdfFile.open();
+        } catch (Exception ex) {
+            CoreInformationMessages.HDF5Info("Failed to open " + file.getName() +  ": " + ex.getMessage());
+            return;
+        } 
+        
+        try {
+            Group root = (Group) ((javax.swing.tree.DefaultMutableTreeNode) hdfFile.getRootNode()).getUserObject();
+            // create DatasetTimp group at the root
+            Group groupTimp = hdfFile.createGroup("DatasetTimp", root);
+
+            //create data type and store the data
+            long[] data_dim = {dataset.getNl(), dataset.getNt()};
+            Datatype dtype = hdfFile.createDatatype(Datatype.CLASS_FLOAT, 8, Datatype.NATIVE, Datatype.NATIVE);
+            Dataset dset = hdfFile.createScalarDS("psisim", groupTimp, dtype, data_dim, data_dim, null, 0, dataset.getPsisim());
         } catch (Exception ex) {
             CoreInformationMessages.HDF5Info(file.getName() +  ": " + ex.getMessage());
         } finally {
-            CoreInformationMessages.HDF5Info("Saving to TIMP dataset \"" + dataset.getDatasetName() + "\" to HDF5 file " + file.getCanonicalPath());
+            try {
+                hdfFile.close();
+                CoreInformationMessages.HDF5Info("Saved TIMP dataset \"" + dataset.getDatasetName() + "\" to HDF5 file " + file.getCanonicalPath());
+            } catch (HDF5Exception ex) {
+                CoreInformationMessages.HDF5Info(file.getName() +  ": " + ex.getMessage());
+            }
         }
     }
 }
